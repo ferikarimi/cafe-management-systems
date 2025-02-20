ac448a066d57bc19ee996b5675eaeba244be0457
behradsh <behradsh10@gmail.com>
2025-02-20 10:42:44 +0330

    delete project from develop

--- myproject/menu/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import MenuItems, Category
-# Register your models here.
-admin.site.register(MenuItems)
-admin.site.register(Category)
--- myproject/menu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import menu, item_detail, add_to_cart
-
-urlpatterns = [
-    path('menu/', menu, name='menu'),  # URL for the menu page
-    path('menu/item/<int:item_id>/', item_detail, name='item_detail'),  # URL for item detail
-    path('menu/add/<int:item_id>/', add_to_cart, name='add_to_cart'),  # URL to add item to cart
-]

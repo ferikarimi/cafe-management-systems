fdd5001f29d64532df4317bd6c70c8e899f15d12
ferikarimi <farzad3467@mail.com>
2025-02-27 12:04:09 +0330

    Save local changes before pull

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -130,12 +130,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-<<<<<<< HEAD
 
-=======
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
->>>>>>> develop
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

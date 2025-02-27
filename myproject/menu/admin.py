78ce36798445f595d618693aa092c04afe53814a
feri <117574027+ferikarimi@users.noreply.github.com>
2025-02-27 08:56:58 +0330

    admin.py changed for add | edit | delete

--- myproject/menu/admin.py
+++ myproject/menu/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import MenuItems, Category
 # Register your models here.
-admin.site.register(MenuItems)
+
+class MenuItemsAdmin(admin.ModelAdmin) :
+    list_display = ('name' ,'price' ,'category' , 'discount' ,'serving_time_period' , 'estimated_cooking_time' )
+    search_fields = ('name' ,'category')
+    list_filter = ('category' ,'discount')
+
+
+admin.site.register(MenuItems , MenuItemsAdmin)
 admin.site.register(Category)

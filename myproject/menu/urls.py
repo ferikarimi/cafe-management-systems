f069678aea11a82e05dc2205a994af8c07c36435
ferikarimi <farzad3467@mail.com>
2025-02-27 10:06:57 +0330

    picture added

--- myproject/menu/urls.py
+++ myproject/menu/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import menu, menu_search, menu_filter, item_detail, add_to_cart
+from .views import menu, menu_search, menu_filter, item_detail, add_to_cart , edit_menu_item , delete_menu_item , add_menu_item
 
 urlpatterns = [
     path('', menu, name='menu'),  # URL for the menu page
@@ -7,4 +7,7 @@ urlpatterns = [
     path('add/<int:item_id>/', add_to_cart, name='add_to_cart'),  # URL to add item to cart
     path('search/' , menu_search , name='menu_search'),
     path('filter/' , menu_filter , name='menu_filter'),
+    path('add/' , add_menu_item , name='add_menu_item'),
+    path('edit/<int:item_id>' , edit_menu_item , name='edit_menu_item'),
+    path('delete/<int:item_id>' , delete_menu_item , name='delete_menu_item'),
 ]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = str(Path(__file__).resolve().parent.parent)
-
+# BASE_DIR = str(Path(__file__).resolve().parent.parent)
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cafeprojectgroupdb',
-        'USER': 'postgres',
-        'PASSWORD': 'behrad',
+        'NAME': 'cafe_project_db',
+        'USER': 'farzad3467',
+        'PASSWORD': 'FARZAD228',
         'HOST': 'localhost',
         'PORT': '5432'
     }
@@ -125,12 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+# STATIC_URL = os.path.join(BASE_DIR, '/static/')
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

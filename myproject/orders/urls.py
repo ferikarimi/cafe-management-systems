642821fccc2cde41b64e06d076bda3a3555dcd16
Seyyed-Ali99 <hoseinishakor@gmail.com>
2025-03-06 03:20:54 +0330

    templates created and some changes modified

--- myproject/orders/urls.py
+++ myproject/orders/urls.py
@@ -31,19 +31,19 @@ urlpatterns = [
     path('menu_item/<int:item_id>/delete' , delete_menu_item , name='delete_menu_item'),
 
     path("orders/",show_order_list,name="orders"),
-    path("order/<int:order_id>",show_order_indetail,name="order"),
+    # path("order/<int:order_id>",show_order_indetail,name="order"),
     path("create_order/",create_order_view,name="create_order"),
     path("edit_order/<int:order_id>",order_update_view,name="update_order"),
     path("delete_order/<int:order_id>",order_delete_view,name="delete_order"),
 
     path("order_details/",order_details_list,name="order_details"),
-    path("order_detail/<int:orderdetail_id>",order_details_indetail,name="order_detail"),
+    # path("order_detail/<int:orderdetail_id>",order_details_indetail,name="order_detail"),
     path("create_order_detail/",create_orderdetail,name="create_order_detail"),
     path("edit_order_detail/<int:orderdetail_id>",edit_orderdetail,name="orderdetail_edit"),
     path("delete_order_detail/<int:orderdetail_id>",delete_orderdetail,name="delete_orderdetail"),
 
     path("receipts/",receipt_show_list,name="receipt_list"),
-    path("receipt/<int:receipt_id>",receipt_show_indetail,name="receipt"),
+    # path("receipt/<int:receipt_id>",receipt_show_indetail,name="receipt"),
     path("create_receipt/",create_receipt,name="create_receipt"),
     path("edit_receipt/<int:receipt_id>",update_receipt,name="edit_receipt"),
     path("delete_receipt/<int:receipt_id>",delete_receipt,name="delete_receipt"),
--- myproject/orders/views.py
+++ myproject/orders/views.py
@@ -40,23 +40,23 @@ def table_delete_view(request,table_id):
 def show_order_list(request):
     orders_obj = Orders.objects.all()
     context = {"orders":orders_obj}
-    return render(request,"/",context=context)
+    return render(request,"orders/orders.html",context=context)
 
 def show_order_indetail(request,order_id):
     orders_indetail_obj = Orders.objects.get(id=order_id)
-    context = {"order_indetail":orders_indetail_obj}
-    return render(request,"/",context=context)
+    context = {"order":orders_indetail_obj}
+    return render(request,"orders/",context=context)
 
 def create_order_view(request):
     if request.method == "POST":
         form = OrdersForm(request.POST)
         if form.is_valid():
             form.save()
-            redirect("")
+            redirect("orders")
         else:
             form = OrdersForm()
 
-        return render(request,"/",{"form":form})
+        return render(request,"orders/create_order.html",{"form":form})
 
 def order_update_view(request,order_id):
     context = {}
@@ -64,38 +64,38 @@ def order_update_view(request,order_id):
     form = OrdersForm(instance=order_obj)
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect("")
+        redirect("orders")
     context['form'] = form
-    return render(request,"/",context)   
+    return render(request,"orders/edit_order.html",context)   
 
 def order_delete_view(request,order_id):
     context = {}
     order_obj = get_object_or_404(Orders,id=order_id)
     if request.method == "POST":
         order_obj.delete()
-        return HttpResponseRedirect("/")
-    return render(request,"/",context=context)
+        redirect("orders")
+    return render(request,"orders/delete_order.html",context=context)
 
 def order_details_list(request):
     order_details_obj = OrdersDetails.objects.all()
-    context = order_details_obj
-    return render(request,"/",context=context)
+    context = {"orderdetails":order_details_obj}
+    return render(request,"orders/orderdetails.html",context=context)
 
 def order_details_indetail(request,orderdetail_id):
     order_details_obj = OrdersDetails.objects.get(id=orderdetail_id)
-    context = order_details_obj
-    return render(request,"/",context=context)
+    context = {"orderdetail":order_details_obj}
+    return render(request,"orders/",context=context)
 
 def create_orderdetail(request):
     if request.method == "POST":
         form = OrderDetailsForm(request.POST)
         if form.is_valid():
             form.save()
-            redirect("/")
+            redirect("order_details")
         else :
             form = OrderDetailsForm()  
 
-        return render(request,"/",{"form":form})    
+        return render(request,"orders/create_orderdetail.html",{"form":form})    
 
 def edit_orderdetail(request,orderdetail_id):
     context = {}
@@ -103,37 +103,37 @@ def edit_orderdetail(request,orderdetail_id):
     form = OrderDetailsForm(instance=orderdetail_obj)
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect("")
+        redirect("order_details")
     context['form'] = form
-    return render(request,"/",context)    
+    return render(request,"orders/edit_orderdetail.html",context=context)    
 
 def delete_orderdetail(request,orderdetail_id):
     context = {}
     orderdetail_obj = get_object_or_404(Orders,id=orderdetail_id)
     if request.method == "POST":
         orderdetail_obj.delete()
-        return HttpResponseRedirect("/")
-    return render(request,"/",context=context)  
+        redirect("order_details")
+    return render(request,"orders/delete_orderdetail.html",context=context)  
 
 def receipt_show_list(request):
     receipts_obj = Reciepts.objects.all()
-    context = receipts_obj
-    return render(request,"/",context=context)
+    context = {"receipts":receipts_obj}
+    return render(request,"orders/receipts.html",context=context)
 
 def receipt_show_indetail(request,receipt_id):
     receipt_obj = Reciepts.objects.get(id=receipt_id)
-    context = receipt_obj
-    return render(request,"",context=context)
+    context = {"receipts":receipt_obj}
+    return render(request,"orders/",context=context)
 
 def create_receipt(request):
     if request.method == "POST":
         form = ReceiptForm(request.POST)
         if form.is_valid():
             form.save()
-            redirect("")
+            redirect("receipt_list")
         else:
             form = ReceiptForm()
-        return render(request,"/",{"form":form})     
+        return render(request,"orders/create_receipt.html",{"form":form})     
 
 def update_receipt(request,receipt_id):
     context = {}
@@ -141,14 +141,14 @@ def update_receipt(request,receipt_id):
     form = ReceiptForm(instance=receipt_obj)
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect("")
+        redirect("receipt_list")
     context['form'] = form
-    return render(request,"/",context)
+    return render(request,"orders/edit_receipt.html",context)
 
 def delete_receipt(request,receipt_id):
     context = {}
     receipt_obj = get_object_or_404(Reciepts,id=receipt_id)
     if request.method == "POST":
         receipt_obj.delete()
-        return HttpResponseRedirect("/")
-    return render(request,"/",context=context)
+        redirect("receipt_list")
+    return render(request,"orders/delete_receipt.html",context=context)
